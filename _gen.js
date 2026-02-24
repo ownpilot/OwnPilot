a089cc23c64ce31c9e3fbb108db4705798091581
Ersin KOÇ <ersinkoc@gmail.com>
2026-02-24 23:32:38 +0200

    chore: remove temporary helper scripts and test files
    Remove 6 temporary files used during development:
    - _gen.js, _w.mjs, _write-tests.js, _write-tests.mjs (code generation helpers)
    - _test.txt, _tiny.txt, _tiny2.txt (test data files)

--- _gen.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const Q = String.fromCharCode(39);
-function q(s) {
-  return Q + s + Q;
-}
-const p = process.argv[2];
-const c = fs.readFileSync(process.argv[3], 'utf8').replace(/QQQQ/g, Q);
-fs.writeFileSync(p, c);
-console.log('Written ' + c.length + ' chars to ' + p);
--- _w.mjs
+++
@@ -1,3 +0,0 @@
-import{writeFileSync as w}from"fs";const Q=String.fromCharCode(39);function q(s){return Q+s+Q}let L=[];process.stdin.on("data",d=>L.push(d.toString()));process.stdin.on("end",()=>{const c=L.join("").split("
-").map(l=>l.replace(/«/g,Q)).join("
-");w(process.argv[2],c);console.log("Written",c.length,"chars")});
